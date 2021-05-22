dbaf77e28ea893ec77d363906f8c0a8ea4941a63
NovikovNik <nowickow.kol@yandex.ru>
2021-05-22 12:30:32 +0300

    TestNG branch added

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.Test
+
+    public void ifZeroAmount(){
+        Assert.assertEquals(service.remain(0),1000);
+    }
+
+    @org.testng.annotations.Test
+
+    public void ifThousandAmount(){
+        Assert.assertEquals(service.remain(1000),1000);
+    }
+
+    @org.testng.annotations.Test
+
+    public void ifMoreThanThousandAmount(){
+        Assert.assertEquals(service.remain(1500),500);
+    }
+
+    @org.testng.annotations.Test
+    public void ifMinusValueAmount(){
+        Assert.assertEquals(service.remain(-1000),1000);
+    }
+
+}
