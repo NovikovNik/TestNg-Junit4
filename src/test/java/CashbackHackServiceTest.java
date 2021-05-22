ad4db7b6965d4a12ac5344ae7a83e67ab62739ce
NovikovNik <nowickow.kol@yandex.ru>
2021-05-22 12:35:38 +0300

    Junit4 Tests

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -7,27 +7,27 @@ import static org.testng.Assert.*;
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void ifZeroAmount(){
-        Assert.assertEquals(service.remain(0),1000);
+        org.junit.Assert.assertEquals(service.remain(0),1000);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void ifThousandAmount(){
-        Assert.assertEquals(service.remain(1000),1000);
+        org.junit.Assert.assertEquals(service.remain(1000),0);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void ifMoreThanThousandAmount(){
-        Assert.assertEquals(service.remain(1500),500);
+        org.junit.Assert.assertEquals(service.remain(1500),500);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void ifMinusValueAmount(){
-        Assert.assertEquals(service.remain(-1000),1000);
+        org.junit.Assert.assertEquals(service.remain(-1000),1000);
     }
 
 }
