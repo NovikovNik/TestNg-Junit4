e59159a759062b64b13017136195bc67e4d7c443
NovikovNik <nowickow.kol@yandex.ru>
2021-05-22 13:36:34 +0300

    garbage cleaned

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,33 +1,28 @@
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
-
-
-import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @org.junit.Test
 
-    public void ifZeroAmount(){
-        org.junit.Assert.assertEquals(1000,service.remain(0));
+    public void ifZeroAmount() {
+        org.junit.Assert.assertEquals(1000, service.remain(0));
     }
 
     @org.junit.Test
 
-    public void ifThousandAmount(){
-        org.junit.Assert.assertEquals(0,service.remain(1000));
+    public void ifThousandAmount() {
+        org.junit.Assert.assertEquals(0, service.remain(1000));
     }
 
     @org.junit.Test
 
-    public void ifMoreThanThousandAmount(){
-        org.junit.Assert.assertEquals(500,service.remain(1500));
+    public void ifMoreThanThousandAmount() {
+        org.junit.Assert.assertEquals(500, service.remain(1500));
     }
 
     @org.junit.Test
-    public void ifMinusValueAmount(){
-        org.junit.Assert.assertEquals(1000,service.remain(-1000));
+    public void ifMinusValueAmount() {
+        org.junit.Assert.assertEquals(1000, service.remain(-1000));
     }
 
 }
