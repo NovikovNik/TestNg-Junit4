d473b431a9da49be44d14a5080db34c8458dfcd1
NovikovNik <nowickow.kol@yandex.ru>
2021-05-22 13:27:28 +0300

    Junit4 + Jupiter

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,8 +1,6 @@
-import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.junit.jupiter.api.Test;
 
-
-import static org.testng.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
@@ -28,6 +26,29 @@ public class CashbackHackServiceTest {
     @org.junit.Test
     public void ifMinusValueAmount(){
         org.junit.Assert.assertEquals(1000,service.remain(-1000));
+    }
+
+    @Test
+
+    public void ifZeroAmountJupiterApi(){
+        assertEquals(1000,service.remain(0));
+    }
+
+    @Test
+
+    public void ifThousandAmountJupiterApi(){
+        assertEquals(0,service.remain(1000));
+    }
+
+    @Test
+
+    public void ifMoreThanThousandAmountJupiterApi(){
+        assertEquals(500,service.remain(1500));
+    }
+
+    @Test
+    public void ifMinusValueAmountJupiterApi(){
+        assertEquals(1000,service.remain(-1000));
     }
 
 }
